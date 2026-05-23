c12f6098fd9452eac3c82c4cc43267cbda1513e1
bangburgerstf-creator <bangburgerstf@gmail.com>
2026-05-23 11:45:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const form = document.getElementById("contact-form");
+const form =
+  document.getElementById("contact-form");
 
 const successMessage =
   document.getElementById("success-message");
@@ -43,3 +44,28 @@ form.addEventListener("submit", async function(e) {
   }
 
 });
+
+const observer =
+  new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+      if(entry.isIntersecting) {
+
+        entry.target.classList.add("show");
+
+      }
+
+    });
+
+  }, {
+    threshold: 0.2
+  });
+
+document
+  .querySelectorAll(".fade-up, .feature-card")
+  .forEach((el) => {
+
+    observer.observe(el);
+
+  });
